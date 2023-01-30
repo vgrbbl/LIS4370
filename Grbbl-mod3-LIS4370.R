20cf5bbdb0153e05d87f58f290101a10e218c894
vgrbbl <108891822+vgrbbl@users.noreply.github.com>
2023-01-30 03:35:10 -0500

    Add files via upload

--- Grbbl-mod3-LIS4370.R
+++ Grbbl-mod3-LIS4370.R
@@ -1,7 +1,6 @@
-# Module 3
-# Data frames
+# Module 3: Data frames
 
-Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
 
 ABC <- c(4, 62, 51, 21, 2, 14, 15)
 
@@ -11,14 +10,28 @@ polls <- data.frame(Name,ABC,CBS, stringsAsFactors = FALSE)
 
 polls
 
-
 # Dataframe Output 
 
-# Name ABC CBS
+#       Name ABC CBS
 # 1     Jeb   4  12
 # 2  Donald  62  75
 # 3     Ted  51  43
 # 4   Marco  21  19
 # 5   Carly   2   1
 # 6 Hillary  14  21
-# 7  Berine  15  19
+# 7  Bernie  15  19
+
+meanCols <- colMeans(polls[,2:3])
+meanCols
+
+# Output of column mean
+# ABC      CBS 
+# 24.14286 27.14286 
+
+meanRows <- rowMeans(polls[1:7,-1])
+meanRows
+
+# Output of row means
+# 1    2    3    4    5    6    7 
+# 8.0 68.5 47.0 20.0  1.5 17.5 17.0 
+
